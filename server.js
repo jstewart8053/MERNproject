dd32030d8db3b527ee1c54c7a202ed3ada9dafbd
Jennifer Stewart <jstewart8053@gmail.com>
2020-11-06 23:15:04 -0600

    creating backend

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
