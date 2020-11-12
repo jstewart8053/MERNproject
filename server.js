a402fe8f327f14286ee90c3688658be67e77e40e
Jennifer Stewart <jstewart8053@gmail.com>
2020-11-11 20:11:25 -0600

    added body-parser and method-over-ride

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -25,5 +31,6 @@ db.once('error', error => console.log("connected to Mongoose"))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+// app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
