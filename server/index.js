e520269e06dc32d5e5323c6005d01754ecbd4ec9
Jennifer Stewart <jstewart8053@gmail.com>
2020-11-13 23:59:03 -0600

    fixed typo in index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const CONNECTION_URL = "mongodb+srv://user:Malayah09@cluster0.buw8j.mongodb.net/
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () -> console.log(`Server running on port: ${PORT}`)))
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
