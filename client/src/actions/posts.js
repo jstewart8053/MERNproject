3972774f17e1e07cbcbaa0d691c73929dafddca0
Jennifer Stewart <jstewart8053@gmail.com>
2020-11-24 05:24:29 -0600

    set up Redux and added Provider and store to App.js

---
+++ client/src/actions/posts.js
@@ -0,0 +1,12 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import * as api from '../api/index.js';
+
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+
+        dispatch({ type: FETCH_ALL, payload: data });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
