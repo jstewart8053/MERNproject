eab2ea813821604c0e9599f6137e442ab4318176
Jennifer Stewart <jstewart8053@gmail.com>
2020-11-23 21:53:33 -0600

    set-up Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
 
 import App from './App';
 
-
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
-
-  <App />,
-
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
 // If you want to start measuring performance in your app, pass a function
