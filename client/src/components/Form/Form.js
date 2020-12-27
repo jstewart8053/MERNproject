f3c0294bea4a1d7f0fa2c4dc95d40947287fceb2
Jen Stewart <jstewart8053@gmail.comm>
2020-12-26 20:50:33 -0600

    feat: add to Form and MaterialUi to markup

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,15 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import useStyles from './styles'
-
+import React, { useState, useEffect } from "react";
+import useStyles from "./styles";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from "react-file-base64";
 
 function Form() {
+  const [postData, setPostData] = useState({
+    creator: " ",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const classes = useStyles();
 
+  const handleSubmit = () => {};
 
-    return (
-        <div>
-            <h1>Form</h1>
+  const clear = () => {};
+
+  return (
+    <Paper>
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          value={postData.creator}
+          fullWidthvalue
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          value={postData.title}
+          fullWidthvalue
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          value={postData.message}
+          fullWidthvalue
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          value={postData.tags}
+          fullWidthvalue
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+          <Button
+            className={classes.buttonSubmit}
+            variant="contained"
+            color="primary"
+            size="large"
+            type="submit"
+            fullWidth
+          >
+            Submit
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="small"
+            onClick={clear}
+            fullWidth
+          >
+            Clear
+          </Button>
         </div>
-    )
+      </form>
+    </Paper>
+  );
 }
 
 export default Form;
--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const { urlencoded } = require("body-parser");
 
-const postRoutes = require('./routes/posts.js')
+const postRoutes = require("./routes/posts.js");
 
 const app = express();
 
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.use("/posts", postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://user:Malayah09@cluster0.buw8j.mongodb.net/test?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000
+const CONNECTION_URL =
+  "mongodb+srv://user:Malayah09@cluster0.buw8j.mongodb.net/test?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
 //https://cloud.mongodb.com/v2/5f950b83f942f705fc72679e#clusters
