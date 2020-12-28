39c4c884671f65855198a74799a2112a49b189fb
Jen Stewart <jstewart8053@gmail.com>
2020-12-28 02:52:07 -0600

    add: adds Grid and Circular progress from MaterialUi, issue in Post.js with Material Icons

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import useStyles from './styles'
-import { useDispatch } from 'react-redux';
+import React from "react";
+import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+// import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+// import DeleteIcon from "@material-ui/icons/Delete";
+// import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Post() {
-    const dispatch = useDispatch();
-    const classes = useStyles()
-    return (
-        <div>
-            <h1 className={classes}>
-                Post</h1>
-        </div>
-    )
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  return (
+    <div>
+      <h1 className={classes}>Post</h1>
+    </div>
+  );
 }
 
 export default Post;
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,20 +1,28 @@
 import React from "react";
 import Post from "./Post/Post";
 import useStyles from "./styles";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 function Posts() {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
-  console.log(posts);
-
-  return (
-    <div>
-      <h1> Posts </h1>
-      <Post />
-      <Post />
-    </div>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => {
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>;
+      })}
+    </Grid>
   );
 }
 
